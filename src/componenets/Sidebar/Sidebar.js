ba6347d166587e3a7b89b9744c266940b3a9edb0
Abdul Moiz <moiza0799@gmail.com>
2022-11-16 22:47:38 +0500

    fix: updated side bar text

--- src/componenets/Sidebar/Sidebar.js
+++ src/componenets/Sidebar/Sidebar.js
@@ -59,7 +59,7 @@ export default function Sidebar({ isAdmin }) {
                     <Link
                       className={
                         'text-xs uppercase py-3 font-bold block ' +
-                        (window.location.href.indexOf('/OrderStatus') !== -1
+                        (window.location.href.indexOf('/LoadStatus') !== -1
                           ? 'text-lightBlue-500 hover:text-lightBlue-600'
                           : 'text-blueGray-700 hover:text-blueGray-500')
                       }
@@ -68,12 +68,12 @@ export default function Sidebar({ isAdmin }) {
                       <i
                         className={
                           'fas fa-tv mr-2 text-sm ' +
-                          (window.location.href.indexOf('/CreateShipment') !== -1
+                          (window.location.href.indexOf('/LoadStatus') !== -1
                             ? 'opacity-75'
                             : 'text-blueGray-300')
                         }
                       ></i>{' '}
-                      Order status
+                     load statuss
                     </Link>
                   </li>
                   <li className="items-center">
