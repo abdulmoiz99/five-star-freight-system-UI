729bece470c6b5d68054287692c3687752ab2f24
Abdul Moiz <moiza0799@gmail.com>
2022-11-23 21:28:28 +0500

    feat: carrier assignment to shipper completed

--- src/componenets/Tables/AssignCarrierTable.js
+++ src/componenets/Tables/AssignCarrierTable.js
@@ -1,21 +1,30 @@
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCircle } from '@fortawesome/free-solid-svg-icons'
 import { getStorage } from '../../shared/LoacalStorage'
 import { Link } from 'react-router-dom'
+import Alert from '../Alerts/Alert'
 
 export class AssignCarrierTable extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      displayAlert: false,
+      success: true,
+      AlertMessage: '',
+      Processing: true,
+    }
     this.state = { report: [], reportList: [], loading: true }
+    this.state = { assignCarrier: [] }
   }
   componentDidMount() {
-    // this.populateTableData()
+    this.setState({ GeneratingReport: false })
+    this.populateTableData()
   }
   populateTableData = async () => {
     let token = getStorage('token')
+    const id = new URLSearchParams(window.location.search).get('id');
+    let carriers;
     const response = await fetch(
-      'https://fivestartlogisticsapi.azurewebsites.net/api/Shipment/orders',
+      `https://fivestartlogisticsapi.azurewebsites.net/api/Admin/carriers?shipperId=${id}`,
       {
         headers: { Authorization: `Bearer ${token}` },
       },
@@ -24,86 +33,84 @@ export class AssignCarrierTable extends React.Component {
     this.setState({
       reportList: data.result,
       loading: false,
+    }, () => {
+      carriers = this.state.reportList.map((element) => ({ id: element.id, isAssign: element.isSelected }))
+      this.setState({ assignCarrier: carriers })
     })
   }
+  upsert = (array, element) => { // (1)
+    const i = array.findIndex(_element => _element.id === element.id);
+    if (i > -1) array[i] = element; // (2)
+    else array.push(element);
+  }
+  handleCheck = (event) => {
+    const obj = { 'id': event.target.id, 'isAssign': event.target.checked };
+    this.upsert(this.state.assignCarrier, obj)
+    console.log(this.state.assignCarrier)
+  }
+  AssignCarriers = async () => {
+    this.setState({ Processing: true })
+    const carrierIds = this.state.assignCarrier.filter(carrier => carrier.isAssign === true).map(c => c.id);
+    console.log(carrierIds)
+    const shipperId = new URLSearchParams(window.location.search).get('id');
+    let token = getStorage('token')
+    try {
+      const body = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          shipperId: shipperId,
+          carrierIds: carrierIds,
+        }),
+      }
+      const response = await fetch(
+        'https://fivestartlogisticsapi.azurewebsites.net/api/Admin/assign-carriers',
+        body,
+      )
+      const data = await response.json()
+      if (data.success === true) {
+        this.setState({ displayAlert: true, AlertMessage: "Carrier assigned successfully.", success: true, })
+      } else if (data.success === false) {
+        this.setState({ displayAlert: true, AlertMessage: data.errors[0], success: false, })
+      }
+    } catch (e) {
+      console.log(e)
+    }
+    this.setState({ Processing: false })
+
+  }
+
   reportReportList(reportList) {
     return (
       <>
-        <tr>
-          <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-sm whitespace-nowrap p-4 text-left flex items-center">
-            <span className="ml-3 font-bold text-blueGray-600"  >
-              Carrier 1
-            </span>
-          </th>
-          <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-sm whitespace-nowrap p-4">
-            001122
-          </td>
-          <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-sm whitespace-nowrap p-4">
-            98567
-          </td>
-
-          <td className="border-t-0  align-middle  text-sm whitespace-nowrap p-5">
-            <label className="cursor-pointer">
-              <input
-                id="customCheckLogin"
-                type="checkbox"
-                name='isLoadQuoted'
-                value={this.state.isLoadQuoted}
-                onChange={this.handleCheck}
-                className="form-checkbox border border-gray-300 rounded text-blueGray-700 ml-1 w-5 h-5 ease-linear transition-all duration-150"
-              />
-            </label>
-          </td>
-        </tr>
-        <tr>
-          <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-sm whitespace-nowrap p-4 text-left flex items-center">
-            <span className="ml-3 font-bold text-blueGray-600"  >
-              Carrier 2
-            </span>
-          </th>
-          <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-sm whitespace-nowrap p-4">
-            001122
-          </td>
-          <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-sm whitespace-nowrap p-4">
-            98567
-          </td>
-
-          <td className="border-t-0  align-middle  text-sm whitespace-nowrap p-5">
-            <label className="cursor-pointer">
-              <input
-                id="customCheckLogin"
-                type="checkbox"
-                name='isLoadQuoted'
-                value={this.state.isLoadQuoted}
-                onChange={this.handleCheck}
-                className="form-checkbox border border-gray-300 rounded text-blueGray-700 ml-1 w-5 h-5 ease-linear transition-all duration-150"
-              />
-            </label>
-          </td>
-        </tr>
         {reportList?.map((report) => (
           <tr key={report.id}>
             <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-sm whitespace-nowrap p-4 text-left flex items-center">
               <span className="ml-3 font-bold text-blueGray-600"  >
-                {report.purchaseOrderNumber}
+                {report.name}
               </span>
             </th>
             <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-sm whitespace-nowrap p-4">
-              {report.pickUpDateTime}
+              {report.mcNumber}
             </td>
             <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-sm whitespace-nowrap p-4">
-              {report.deliveryDateTime}
+              {report.dotNumber}
             </td>
-            <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-sm whitespace-nowrap p-4">
-              <FontAwesomeIcon icon={faCircle} color="#1AFC3F" /> delivered
-            </td>
-            <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-lg whitespace-nowrap p-4">
-              <Link
-                to={"/CarrierDetails?id=" + report.id}
-                className="bg-lightBlue-500 text-white active:bg-lightBlue-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-              >
-                View Details
-              </Link>
+            <td className="border-t-0  align-middle  text-sm whitespace-nowrap p-5">
+              <label className="cursor-pointer">
+                <input
+                  id={report.id}
+                  type="checkbox"
+                  name='isLoadQuoted'
+                  defaultChecked={report.isSelected}
+                  value={this.state.isLoadQuoted}
+                  onChange={this.handleCheck}
+                  className="form-checkbox border border-gray-300 rounded text-blueGray-700 ml-1 w-5 h-5 ease-linear transition-all duration-150"
+                />
+              </label>
             </td>
           </tr>
         ))
@@ -111,18 +118,26 @@ export class AssignCarrierTable extends React.Component {
       </>
     )
   }
-
+  renderAlert = () => {
+    if (this.state.displayAlert) {
+      return (
+        <>
+          <Alert message={this.state.AlertMessage} success={this.state.success} />
+        </>
+      )
+    }
+  }
   render() {
     return (
       <>
         <div className="relative flex flex-col min-w-0 break-words w-full  lg:w-6/12 mb-6 shadow-lg rounded bg-white" >
+          {this.renderAlert()}
           <div className="rounded-t mb-0 px-4 py-3 border-0">
             <div className="flex flex-wrap items-center">
               <div className="relative w-full px-4 max-w-full flex-grow flex-1">
                 <h3 className="font-bold text-lg text-blueGray-700"  >
                   Shipper: Shipper 1
                 </h3>
-
               </div>
               <Link
                 to="/Shippers"
@@ -130,14 +145,28 @@ export class AssignCarrierTable extends React.Component {
               >
                 View Shippers
               </Link>
-              <button className="bg-lightBlue-500 text-white active:bg-lightBlue-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150" >
+              {!this.state.Processing ? (
+                <>
+                   <button
+                className="bg-lightBlue-500 text-white active:bg-lightBlue-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
+                onClick={this.AssignCarriers}
+              >
                 Update
               </button>
+                </>
+              ) : (
+                <button
+                className="bg-blueGray-700 text-white active:bg-lightBlue-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
+              >
+                Updating...
+              </button>
+              )}
             </div>
           </div>
+
           <div className="block w-full overflow-x-auto">
             {/* Projects table */}
-            <table className="items-center w-full bg-transparent border-collapse">
+            <table className="items-center w-full bg-transparent border-collapse" id="assignCarrier-dataset">
               <thead>
                 <tr>
                   <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left bg-blueGray-50 text-blueGray-500 border-blueGray-100">
